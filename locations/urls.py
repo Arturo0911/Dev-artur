e8c3e8e5ef9d5c491bc210a1483ad94806d8ed77
Arturo <anegreiross@outlook.com>
2020-08-10 03:44:44 -0500

    modified models.py

--- locations/urls.py
+++ locations/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home_page, name = 'home_page')#, name= 'home')  # from url
+    path('', views.home_page, name = 'home_page'),
+    path('<int:question_id_>/', views.details_page, name= "details"),
+    path('<int:question_id_>/results/', views.results_page, name= "results"),
+    path('<int:question_id_>/vote/', views.vote, name= "vote"),
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Question
 
 
 def home_page(request):
+    #response = "<strong>This is the home page. </strong>"
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    teamplate = loader.get_template('locations/index.html')
 
-    a = 10
-    html = ""
-    for x in range(0,a+1):
-        html = html + str(x)
-    response = "<strong>This is the home page. </strong> %s"%html
-    return HttpResponse(response)
+    context = {
+        'lastest_question_list': lastest_question_list,
+    }
+
+
+    #output = ', '.join([q.question_text for q in lastest_question_list])
+    #return HttpResponse(teamplate.render(context, request))
+    return render(request,'locations/index.html', context )
+
+def details_page(request, question_id_):
+
+
+    try:
+        question = Question.objects.get(pk = question_id_)
+
+    except Question.DoesNotExist:
+        raise Http404("Question doesn't exist")
+
+
+
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results_page(request, question_id_):
+
+    return HttpResponse("Estás buscando los resultados de la pregunta %s"%question_id_)
+
+def vote(request, question_id_):
+
+    return HttpResponse("Estás buscando los votos de la pregunta %s"%question_id_)
+
 
 
 # Create your views here.
